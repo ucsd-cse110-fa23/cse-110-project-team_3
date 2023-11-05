f1119667d969d1ee971bba90ee5b0b6e288e3708
Matthew Yang <myang98732190@gmail.com>
2023-11-05 10:59:54 -0800

    Created Framework for Recipe List UI

---
+++ src/client/App.java
@@ -0,0 +1,58 @@
+package client;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.TextAlignment;
+import javafx.geometry.Insets;
+import javafx.scene.text.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+class Recipe extends HBox {
+
+}
+
+class RecipeList extends VBox {
+
+}
+
+class Footer extends HBox {
+
+}
+
+class Header extends HBox {
+
+}
+
+class AppFrame extends BorderPane {
+
+}
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
