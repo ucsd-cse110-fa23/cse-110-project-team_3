31e994797953cb47812eb51358189b9decf09f91
sminowada <97696723+sminowada@users.noreply.github.com>
2023-11-08 22:13:08 -0800

    Added save/discard buttons

--- src/RecipeView/Main.java
+++ src/RecipeView/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.scene.control.ScrollPane;
 import javafx.geometry.Insets;
@@ -21,11 +22,32 @@ class Recipe extends VBox {
 }
 
 class Footer extends HBox {
+    private Button saveRecipeButton;
+    private Button discardRecipeButton;
 
     Footer() {
         this.setPrefSize(500, 60);
         this.setStyle("-fx-background-color: #F0F8FF;");
         this.setSpacing(15);
+
+        saveRecipeButton = new Button("Save Recipe");
+        saveRecipeButton.setPrefSize(100, 20);
+
+        discardRecipeButton = new Button("Discard Recipe");
+        discardRecipeButton.setPrefSize(100, 20);
+
+        this.getChildren().add(saveRecipeButton);
+        this.getChildren().add(discardRecipeButton);
+        this.setAlignment(Pos.CENTER);
+
+    }
+
+    public Button getSaveRecipeButton() {
+        return saveRecipeButton;
+    }
+
+    public Button getDiscardRecipeButton() {
+        return discardRecipeButton;
     }
 }
 
@@ -42,14 +64,16 @@ class Header extends HBox {
     }
 }
 
-class AppFrame extends BorderPane{
+class AppFrame extends BorderPane {
 
     private Header header;
     private Footer footer;
     private Recipe recipe;
 
-    AppFrame() throws IOException
-    {
+    private Button saveRecipeButton;
+    private Button discardRecipeButton;
+
+    AppFrame() throws IOException {
 
         String recipeBody = RecipeGeneration.generateFakeRecipe();
 
@@ -58,13 +82,12 @@ class AppFrame extends BorderPane{
 
         // Create a tasklist Object to hold the tasks
         recipe = new Recipe(recipeBody);
-        
+
         // Initialise the Footer Object
         footer = new Footer();
 
         ScrollPane scroll = new ScrollPane();
         scroll.setContent(recipe);
-
 
         // Add header to the top of the BorderPane
         this.setTop(header);
@@ -73,12 +96,22 @@ class AppFrame extends BorderPane{
         // Add footer to the bottom of the BorderPane
         this.setBottom(footer);
 
+        // Initialize Button Variables
+        saveRecipeButton = footer.getSaveRecipeButton();
+        discardRecipeButton = footer.getDiscardRecipeButton();
+
         // Call Event Listeners for the Buttons
         addListeners();
     }
 
     public void addListeners() {
+        saveRecipeButton.setOnAction(e -> {
+            System.out.println("Save Recipe Button Clicked");
+        });
 
+        discardRecipeButton.setOnAction(e -> {
+            System.out.println("Discard Recipe Button Clicked");
+        });
     }
 }
 
@@ -87,7 +120,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        // Setting the Layout of the Window- Should contain a Header, Footer and the TaskList
+        // Setting the Layout of the Window- Should contain a Header, Footer and the
+        // TaskList
         AppFrame root = new AppFrame();
 
         // Set the title of the app
