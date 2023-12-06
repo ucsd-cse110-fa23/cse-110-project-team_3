40188d6c70b82787621f6b0e8ef6f8a0eb1f0cd2
Ben Stirling <abenstirling@pm.me>
2023-12-05 20:28:51 -0800

    HTML, GET options

--- app/src/main/java/cse/project/team_3/server/RequestHandler.java
+++ app/src/main/java/cse/project/team_3/server/RequestHandler.java
@@ -17,56 +17,107 @@ public class RequestHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        String response = "Request Received";
+        String response;
         String method = httpExchange.getRequestMethod();
 
         try {
             if (method.equals("GET")) {
                 response = handleGet(httpExchange);
+                String query = httpExchange.getRequestURI().getRawQuery();
+                if (query != null && query.contains("type=recipeImage")) {
+                    httpExchange.getResponseHeaders().set("Content-Type", "text/html");
+                } else {
+                    httpExchange.getResponseHeaders().set("Content-Type", "text/plain");
+                }
             } else if (method.equals("POST")) {
                 response = handlePost(httpExchange);
+                // Set content type if necessary
             } else if (method.equals("PUT")) {
                 response = handlePut(httpExchange);
+                // Set content type if necessary
             } else {
                 throw new Exception("Not Valid Request Method");
             }
         } catch (Exception e) {
-            System.out.println("An erroneous request");
-            response = e.toString();
+            response = "Error processing request: " + e.getMessage();
+            httpExchange.getResponseHeaders().set("Content-Type", "text/plain");
             e.printStackTrace();
         }
 
-        // Sending back response to the client
-        httpExchange.sendResponseHeaders(200, response.length());
-        OutputStream outStream = httpExchange.getResponseBody();
-        outStream.write(response.getBytes());
-        outStream.close();
-    }
-
-    private String handleGet(HttpExchange httpExchange) throws IOException {
-        String response = "Invalid GET request";
-        URI uri = httpExchange.getRequestURI();
-        String query = uri.getRawQuery();
-        if (query != null) {
-
-            // Get the data from the map
-            String recipeID = parseQueryParam(query, "id");
-
-            // Check if the recipeID is not null
-            if (recipeID != null) {
-                // Retrieve the recipe based on the ID from the data map
-                response = data.getOrDefault(recipeID, "Recipe not found");
-                return response;
-            } else {
-                return "Invalid GET request: Missing 'id' parameter";
-            }
-        } else {
-            return "Invalid GET request";
+        byte[] responseBytes = response.getBytes("UTF-8");
+        httpExchange.sendResponseHeaders(200, responseBytes.length);
+        try (OutputStream outStream = httpExchange.getResponseBody()) {
+            outStream.write(responseBytes);
         }
     }
 
- private String handlePut(HttpExchange httpExchange) {
-     try {  
+    private String handleGet(HttpExchange httpExchange) throws IOException {
+        URI uri = httpExchange.getRequestURI();
+        String query = uri.getRawQuery();
+        if (query == null) {
+            return "Invalid GET request: No query parameters provided";
+        }
+
+        // Check if the GET request is for recipe image
+        if (query.contains("type=recipeImage")) {
+            String recipeID = parseQueryParam(query, "id");
+            if (recipeID == null) {
+                return "Invalid GET request: Missing 'id' parameter for recipe image";
+            }
+            String combinedData = data.get(recipeID);
+            if (combinedData != null && combinedData.contains(";;")) {
+                String[] parts = combinedData.split(";;");
+                String recipeText = parts[0];
+                String imageUrl = parts.length > 1 ? parts[1] : "Image not available";
+                // Generate and return HTML response
+                return generateHtmlResponse(recipeText, imageUrl);
+            } else {
+                return "Recipe not found or image not available";
+            }
+        } else {
+            // Handle other GET requests
+            String recipeID = parseQueryParam(query, "id");
+            if (recipeID == null) {
+                return "Invalid GET request: Missing 'id' parameter";
+            }
+            String response = data.getOrDefault(recipeID, "Recipe not found");
+            return response;
+        }
+    }
+
+    private String generateHtmlResponse(String recipeText, String imageUrl) {
+        String htmlResponse = "<!DOCTYPE html>" +
+                "<html lang='en'>" +
+                "<head>" +
+                "<meta charset='UTF-8'>" +
+                "<meta name='viewport' content='width=device-width, initial-scale=1.0'>" +
+                "<title>Recipe</title>" +
+                "<style>" +
+                "  body { font-family: 'Arial', sans-serif; line-height: 1.6; margin: 0; padding: 0; background: #f4f4f4; }"
+                +
+                "  .container { max-width: 800px; margin: auto; padding: 20px; }" +
+                "  .recipe-title { color: #333; }" +
+                "  .recipe-text { background: #fff; padding: 20px; margin-bottom: 20px; border-radius: 10px; box-shadow: 0 5px 15px rgba(0,0,0,0.1); }"
+                +
+                "  .recipe-image { width: 100%; height: auto; border-radius: 10px; }" +
+                "  .not-available { color: #cc0000; }" +
+                "</style>" +
+                "</head>" +
+                "<body>" +
+                "<div class='container'>" +
+                "<h1 class='recipe-title'>Recipe</h1>" +
+                "<div class='recipe-text'>" + recipeText.replace("\n", "<br>") + "</div>" +
+                (imageUrl.equals("Image not available") ? "<p class='not-available'>Image not available</p>"
+                        : "<img src='" + imageUrl + "' alt='Recipe Image' class='recipe-image'>")
+                +
+                "</div>" +
+                "</body>" +
+                "</html>";
+        return htmlResponse;
+    }
+
+    private String handlePut(HttpExchange httpExchange) {
+        try {
             String transcriptionResult = Whisper.transcribeAudio(combinedAudioFile);
             System.out.println("Transcribed Audio: \n" + transcriptionResult);
 
@@ -74,13 +125,14 @@ public class RequestHandler implements HttpHandler {
             temp = temp.replace(".", ";");
             String[] recipe = temp.split(";", 2);
             String mealType = recipe[0];
-            if(mealType.startsWith("B") || mealType.startsWith("b")) {
+            if (mealType.startsWith("B") || mealType.startsWith("b")) {
                 mealType = "Breakfast";
             }
             System.out.println("Meal Type: " + mealType);
             String ingredients = recipe[1];
 
-            String generatedRecipe = ChatGPT.generateResponse("give me a " + mealType + " recipe using " + ingredients + " without using fractions");
+            String generatedRecipe = ChatGPT.generateResponse(
+                    "give me a " + mealType + " recipe using " + ingredients + " without using fractions");
             System.out.println("Generated Recipe: \n" + generatedRecipe);
 
             String imageURL = DallE.generateImage(generatedRecipe);
@@ -88,16 +140,18 @@ public class RequestHandler implements HttpHandler {
 
             String generatedID = generateUniqueId();
 
+            String recipeLink = generatedRecipe + ";;" + imageURL;
+
             // attaching it to the local data map for testing
-            data.put(generatedID, generatedRecipe);
-    
+            data.put(generatedID, recipeLink);
+
             // GET here using the generated ID
             System.out.println("\nquery: " + generatedID);
             handleGet(httpExchange);
-            
+
             // String retrievedRecipe = data.getOrDefault(generatedID, "Recipe not found");
-            //RecipeImagePair pair = new RecipeImagePair(generatedRecipe, imageURL);
-            //JSONObject pairJson = pairToJson(pair);
+            // RecipeImagePair pair = new RecipeImagePair(generatedRecipe, imageURL);
+            // JSONObject pairJson = pairToJson(pair);
             return generatedRecipe + ";" + mealType.trim() + ";" + ingredients.trim();
         } catch (URISyntaxException e) {
             e.printStackTrace();
@@ -109,9 +163,6 @@ public class RequestHandler implements HttpHandler {
     }
 
     private String parseQueryParam(String query, String paramName) {
-        if (query == null || paramName == null) {
-            return null;
-        }
         String[] pairs = query.split("&");
         for (String pair : pairs) {
             String[] keyValue = pair.split("=");
@@ -176,7 +227,7 @@ public class RequestHandler implements HttpHandler {
         JSONObject json = new JSONObject();
         json.put("recipe", pair.getRecipe());
         json.put("imageURL", pair.getImageUrl());
-        
+
         return json.toString();
     }
 
